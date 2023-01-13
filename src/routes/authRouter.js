940011c4c20c508ab29fc3ddc590ccf2ed361068
mostafa <mostafaac30>
2023-01-13 18:52:27 +0200

    forget + reset request check

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -18,8 +18,28 @@ router.post('/register', [
 });
 
 router.post('/login', authController.login);
-router.post('/forget-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
+router.post('/forget-password', [
+    check('email', 'Please enter a valid email').isEmail(),
+
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    };
+    authController.forgotPassword(req, res);
+});
+router.post('/reset-password', [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('otp', 'Please enter a valid otp').isLength({ min: 4, max: 4 }),
+    check('password', 'Please enter a password length more than 5').isLength({ min: 6 })
+
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    };
+    authController.resetPassword(req, res);
+});
 
 module.exports = router;
 
