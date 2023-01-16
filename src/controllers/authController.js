ba394c747ad1b572dcef9b0047adde793388cd79
mostafa <mostafaac30>
2023-01-16 17:51:45 +0200

    connect to mongoDB Atlas

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -109,57 +109,61 @@ const register = (req, res) => {
     if (!username || !email || !password) {
         return res.status(400).json({ msg: 'Please enter all fields' });
     }
+    try {
+        // Check for existing user
+        User.findOne({ email }).then(async user => {
+            if (user) return res.status(400).json({ msg: 'User already exists' });
+            const info = await judgeController.getSolvedProblemsByUsername(username);
+            const rank = info.matchedUser.profile.ranking;
+            const totalSolvedProblems = info.matchedUser.submitStats.acSubmissionNum[0].count;
+            console.log(rank);
+            console.log(totalSolvedProblems);
+            const newUser = new User({
+                username,
+                email,
+                password,
+                judge,
+                rank,
+                totalSolvedProblems
+            });
 
-    // Check for existing user
-    User.findOne({ email }).then(async user => {
-        if (user) return res.status(400).json({ msg: 'User already exists' });
-        const info = await judgeController.getSolvedProblemsByUsername(username);
-        const rank = info.matchedUser.profile.ranking;
-        const totalSolvedProblems = info.matchedUser.submitStats.acSubmissionNum[0].count;
-        console.log(rank);
-        console.log(totalSolvedProblems);
-        const newUser = new User({
-            username,
-            email,
-            password,
-            judge,
-            rank,
-            totalSolvedProblems
-        });
-
-        // Hash the password
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser.save()
-                    .then(user => {
-                        jwt.sign(
-                            { id: user.id },
-                            process.env.JWT_SECRET,
-                            { expiresIn: 3600 },
-                            (err, token) => {
-                                if (err) throw err;
-                                res.json({
-                                    token,
-                                    user: {
-                                        id: user.id,
-                                        username: user.username,
-                                        email: user.email,
-                                        judge: user.judge,
-                                        rank: user.rank,
-                                        totalSolvedProblems: user.totalSolvedProblems
-                                    }
-                                });
-                            }
-                        )
-                    })
-                    .catch(err => {
-                        res.status(500).json({ error: err.message });
-                    });
+            // Hash the password
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+                            jwt.sign(
+                                { id: user.id },
+                                process.env.JWT_SECRET,
+                                { expiresIn: 3600 },
+                                (err, token) => {
+                                    if (err) throw err;
+                                    res.json({
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            username: user.username,
+                                            email: user.email,
+                                            judge: user.judge,
+                                            rank: user.rank,
+                                            totalSolvedProblems: user.totalSolvedProblems
+                                        }
+                                    });
+                                }
+                            )
+                        })
+                        .catch(err => {
+                            res.status(500).json({ error: err.message });
+                        });
+                });
             });
         });
-    });
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
 
 // @route   POST api/auth/login
@@ -172,34 +176,38 @@ const login = (req, res) => {
     if (!email || !password) {
         return res.status(400).json({ msg: 'Please enter all fields' });
     }
+    try {
+        // Check for existing user
+        User.findOne({ email }).then(user => {
+            if (!user) return res.status(400).json({ msg: 'User does not exist' });
 
-    // Check for existing user
-    User.findOne({ email }).then(user => {
-        if (!user) return res.status(400).json({ msg: 'User does not exist' });
+            // Validate password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
-        // Validate password
-        bcrypt.compare(password, user.password)
-            .then(isMatch => {
-                if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+                    jwt.sign(
+                        { id: user.id },
+                        process.env.JWT_SECRET,
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) throw err;
+                            res.json({
+                                token,
+                                user: {
+                                    id: user.id,
+                                    username: user.username,
+                                    email: user.email
+                                }
+                            });
+                        }
+                    )
+                })
+        })
 
-                jwt.sign(
-                    { id: user.id },
-                    process.env.JWT_SECRET,
-                    { expiresIn: 3600 },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({
-                            token,
-                            user: {
-                                id: user.id,
-                                username: user.username,
-                                email: user.email
-                            }
-                        });
-                    }
-                )
-            })
-    })
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
 
 const checkAuth = (req, res, next) => {
